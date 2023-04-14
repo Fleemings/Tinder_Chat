86647be3be8a83ec7de4cbc230562c441d7af882
Milena Fleming <milenasapucaia29@gmail.com>
2023-04-14 02:38:33 +0100

    build: installed  body-parser

--- tinder-chat/server/index.js
+++ tinder-chat/server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello to my app');
